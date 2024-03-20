676aeaf45bd9c579b24bdf50d9958e2c55fb63f1
manumalotra <malotra@ualberta.ca>
2024-03-20 17:36:39 -0600

    Modified sales_app and created admin user
    In this update:
    - Added some classes and attributes to the sales_app.
    - Migrated update to db.sqlite3.
    - Created admin superuser.

--- c3_project/settings.py
+++ c3_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # C3 Apps
+    'sales_app',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- sales_app/models.py
+++ sales_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Sale(models.Model):
+    sale_id = models.IntegerField()
+    sale_amount = models.DecimalField(max_digits=10, decimal_places = 2)
+    digital_signature = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Sale ID: {self.sale_id}, Sale Amount: {self.sale_amount}, Digital Signature: {self.digital_signature}"
+
+class Customer(models.Model):
+    customer_id = models.IntegerField()
+    customer_name = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    customer_email = models.CharField(max_length=200)
+    customer_address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.customer_name
+
