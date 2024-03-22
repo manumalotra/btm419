bb8f89940179b13f16ba77144b8d1c7b7a8a7545
manumalotra <malotra@ualberta.ca>
2024-03-22 14:41:28 -0600

    Revert "Created sales app homepage"
    This reverts commit 0d30516e937d881e5ab34fcbd6b99a4d38aba7f0.

--- sales_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'sales_app'
-
-urlpatterns = [
-    # Home page
-    path('', views.index, name='index'),
-]
--- sales_app/views.py
+++ sales_app/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-# Create your views here.
-def index(request):
-    """The home page for Sales App."""
-    return render(request, 'sales_app/index.html')
