0d30516e937d881e5ab34fcbd6b99a4d38aba7f0
manumalotra <malotra@ualberta.ca>
2024-03-22 14:10:24 -0600

    Created sales app homepage
    Created index.html, which is the homepage for the sales app. It is currently a basic static webpage, but we will add more features to it later.

--- sales_app/views.py
+++ sales_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+def index(request):
+    """The home page for Sales App."""
+    return render(request, 'sales_app/index.html')
