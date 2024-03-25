3b11ff150b12ea9194c491cb7600f6144902559e
MrAnderson2431 <157548565+MrAnderson2431@users.noreply.github.com>
2024-03-25 03:14:52 -0600

    Properly Uploaded Project Folder
    Made error in last commit with adding the files in the project folder instead of the folder.

--- c3_project/settings.py
+++ c3_project/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-MEDIA_URL = '/media/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # C3 Apps
-    'main_homepage_app',
     'sales_app',
     'claims_app',
 
@@ -122,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
