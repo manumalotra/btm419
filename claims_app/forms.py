b19559f843e556aff93577c6fa03b3b353be17c6
MrAnderson2431 <157548565+MrAnderson2431@users.noreply.github.com>
2024-03-27 10:52:06 -0600

    Overhauled sales and connected it to claims

--- claims_app/forms.py
+++ claims_app/forms.py
@@ -12,8 +12,8 @@ class ClaimForm(forms.ModelForm):
 
     class Meta:
         model = Claim
-        fields = ('claim_type', 'claim_desc', 'claim_photo', 'sale_id')
-        exclude = ['claim_id', 'claim_date', 'claim_status']
+        fields = ('claim_type', 'claim_desc', 'claim_photo')
+        exclude = ['claim_id', 'claim_date', 'claim_status', 'deciding_staff', 'sale_id']
         
 class ClaimReviewForm(forms.ModelForm):
     CHOICES1 = (
@@ -26,7 +26,7 @@ class ClaimReviewForm(forms.ModelForm):
     class Meta:
         model = Claim
         fields = ('claim_status',)
-        exclude = ['claim_id', 'claim_type', 'claim_date', 'claim_desc', 'claim_photo', 'sale_id']
+        exclude = ['claim_id', 'claim_type', 'claim_date', 'claim_desc', 'claim_photo', 'sale_id', 'deciding_staff']
 
 class ClaimRepairForm(forms.ModelForm):
     CHOICES2 = (
@@ -39,4 +39,10 @@ class ClaimRepairForm(forms.ModelForm):
     class Meta:
         model = ClaimRepair
         fields = ('repair_status', 'repair_desc', 'repair_photo')
-        exclude = ['repair_id', 'repair_date', 'claim_id']
+        exclude = ['repair_id', 'repair_date', 'claim_id', 'repair_staff']
+
+    def __init__(self, *args, **kwargs):
+        super(ClaimRepairForm, self).__init__(*args, **kwargs)
+        self.fields['repair_photo'].required = False
+
+# Jaiden waz here
--- claims_app/urls.py
+++ claims_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('get_claims/',views.get_claims, name='get_claims'),
     path('get_claims/<int:claim_id>', views.get_claim, name='get_claim'),
-    path('new_claim/', views.new_claim, name='new_claim'),
+    path('new_claim/<int:sale_id>', views.new_claim, name='new_claim'),
     path('claim_repairs/',views.get_claim_repairs, name='get_claim_repairs'),
     path('claim_repairs/<int:repair_id>', views.get_claim_repair, name='get_claim_repair'),
 ]
